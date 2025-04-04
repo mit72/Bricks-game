95f5de934b33da2420d1ff961e164a631a14a93e
Mitja Filej <110605735+mit72@users.noreply.github.com>
2025-04-04 13:05:26 +0200

    Add files via upload

--- js/javascript2.js
+++ js/javascript2.js
@@ -8,37 +8,49 @@ function drawIt() {
     var r = 10;
     var ctx;
     var canvas;
+    var paddlex;
+    var paddleh;
+    var paddlew;
+    let intervalId;
+    var checkEndVar = false;
+
 
     function draw() {
-        ctx.clearRect(0, 0, 300, 300);
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2, true);
-        ctx.closePath();
-        ctx.fill();
-        clear();
 
+        if (checkEndVar) {
+            clearInterval(drawIt);
+            return;
+        }
+
+        clear();
         circle(x, y, 10);
 
-        if (x + dx > WIDTH-r || x + dx < 0+r)
+        rect(paddlex, HEIGHT - paddleh, paddlew, paddleh);
 
+
+        if (x + dx > WIDTH - r || x + dx <  r)
             dx = -dx;
-
-        if (y + dy > HEIGHT-r || y + dy < 0+r)
-
+        if (y + dy < 0 + r)
             dy = -dy;
+        else if (x > paddlex && x < paddlex + paddlew && y > canvas.height - paddleh - r) {
+            dy = -dy ;
+            dx = 8 * ((x - (paddlex + paddlew / 2)) / paddlew);
+            console.log("hit on paddle");
+        } else if (!(x > paddlex && x < paddlex + paddlew) && y > canvas.height - r) {
 
+            checkEndVar = true;
+            drawGameOver();
+        }
         x += dx;
-
         y += dy;
     }
     function init() {
         canvas = document.getElementById('canvas');
         ctx = canvas.getContext('2d');
-        //klic funkcije draw vsakih 10 ms;
         ctx = $('#canvas')[0].getContext("2d");
         WIDTH = $("#canvas").width();
         HEIGHT = $("#canvas").height();
-        return setInterval(draw, 10);
+        return intervalId = setInterval(draw, 10);
     }
 
     function circle(x, y, r) {
@@ -58,15 +70,56 @@ function drawIt() {
     function clear() {
         ctx.clearRect(0, 0, WIDTH, HEIGHT);
     }
-    //END LIBRARY CODE
-    function draw() {
-        clear();
-        circle(x, y, 10);
-        x += dx;
-        y += dy;
+
+
+    function init_paddle() {
+        paddlex = WIDTH / 2;
+        paddleh = 10;
+        paddlew = 75;
     }
 
+    function drawGameOver() {
+        ctx.fillStyle = "red";
+        ctx.font = "1000 100px Arial";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
+    }
+
+    //vlecenje z misko
+    var dragging = false;
+    document.addEventListener("mousedown", function (e) {
+        var relativeX = e.clientX - canvas.offsetLeft;
+        var relativeY = e.clientY - canvas.offsetTop;
+        if (relativeX > paddlex && relativeX < paddlex + paddlew && relativeY > canvas.height - paddleh) {
+            dragging = true;
+        }
+    });
+
+    document.addEventListener("mouseup", function () {
+        dragging = false;
+    });
+
+    document.addEventListener("mousemove", function (e) {
+        var relativeX = e.clientX - canvas.offsetLeft;
+        var relativeY = e.clientY - canvas.offsetTop;
+
+        if (relativeX > paddlex && relativeX < paddlex + paddlew && relativeY > canvas.height - paddleh) {
+            canvas.style.cursor = "pointer";
+        } else {
+            canvas.style.cursor = "default";
+        }
+
+        if (dragging) {
+            if (relativeX > 0 && relativeX < canvas.width) {
+                paddlex = relativeX - paddlew / 2;
+            }
+        }
+    });
+
     init();
+    init_paddle();
+    
 
 
 }
